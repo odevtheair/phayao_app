b0688c4ec7bafcf8722cd565ed1d0d8874bf94ba
Satit Riapit <siteslave@msn.com>
2018-08-21 11:19:38 +0700

    add drawer

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -42,29 +42,108 @@ class _HomePageState extends State<HomePage> {
               }),
         ],
       ),
+      drawer: Drawer(
+        // Add a ListView to the drawer. This ensures the user can scroll
+        // through the options in the Drawer if there isn't enough vertical
+        // space to fit everything.
+        child: ListView(
+          // Important: Remove any padding from the ListView.
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            UserAccountsDrawerHeader(
+              accountName: Text(
+                'Satit Rianpit',
+                style: TextStyle(fontSize: 20.0),
+              ),
+              accountEmail: Text('rianpit@gmail.com'),
+              currentAccountPicture: CircleAvatar(
+                backgroundImage: NetworkImage(
+                    'https://randomuser.me/api/portraits/med/men/90.jpg'),
+              ),
+            ),
+            ListTile(
+              title: Text('ตัวชี้วัดจังหวัด'),
+              subtitle: Text('รายการตัวชี้วัดระดับจังหวัด'),
+              leading: IconButton(icon: Icon(Icons.group), onPressed: () {}),
+              trailing: Icon(Icons.keyboard_arrow_right),
+              onTap: () {
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              title: Text('ตัวชี้วัดเขต'),
+              subtitle: Text('รายการตัวชี้วัดระดับเขต'),
+              leading:
+                  IconButton(icon: Icon(Icons.dashboard), onPressed: () {}),
+              trailing: Icon(Icons.keyboard_arrow_right),
+              onTap: () {
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              title: Text('ตัวชี้วัดกระทรวง'),
+              subtitle: Text('รายการตัวชี้วัดระดับกระทรวง'),
+              leading: IconButton(icon: Icon(Icons.pages), onPressed: () {}),
+              trailing: Icon(Icons.keyboard_arrow_right),
+              onTap: () {
+                Navigator.pop(context);
+              },
+            ),
+          ],
+        ),
+      ),
       body: Padding(
         padding: const EdgeInsets.all(5.0),
         child: Container(
-          height: 100.0,
-          color: Colors.white,
+//          margin: EdgeInsets.all(10.0),
+//          height: 100.0,
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(5.0), color: Colors.white),
           child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: <Widget>[
               Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: <Widget>[
                   Column(
                     children: <Widget>[
-                      Text('xxxxxx'),
-                      Text('bbbbbb'),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Text('กดกหดดหกดกหดดหดหด'),
+                      ),
+                      Text(
+                        'bbbหกดกดbbb',
+                        style: TextStyle(
+                            fontSize: 20.0, fontWeight: FontWeight.bold),
+                      ),
                     ],
                   ),
-                  CircleAvatar(
-                    radius: 40.0,
-                    backgroundColor: Colors.pink,
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: CircleAvatar(
+//                    radius: 40.0,
+                      backgroundImage: NetworkImage(
+                          'https://randomuser.me/api/portraits/med/men/88.jpg'),
+                    ),
                   ),
                 ],
               ),
-              Text('YYYYYYY')
+              Divider(
+                height: 1.0,
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  children: <Widget>[
+                    Icon(Icons.attachment),
+                    SizedBox(
+                      width: 10.0,
+                    ),
+                    Text('กหดกหดหดกหดกหดห'),
+                  ],
+                ),
+              )
             ],
           ),
         ),
